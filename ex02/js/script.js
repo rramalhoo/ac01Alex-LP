184c8d5683d3c8bdfc7efd240d5360af705f4373
Ramalho <159273388+rramalhoo@users.noreply.github.com>
2025-02-24 08:10:55 -0300

    commit

--- ex02/js/script.js
+++
@@ -1,5 +0,0 @@
-celsius = parseFloat(prompt('Digite a temperatura em C:'))
-
-convercao = (celsius * 9 / 5) = 32
-
-alert = (`A temperatura em fahrenheit é: ${convercao}`)
---
+++ ex02/js/script02.js
@@ -0,0 +1,5 @@
+celsius = parseFloat(prompt('Digite a temperatura em C:'));
+
+F = (celsius * 9 / 5) + 32;
+
+alert = (`A temperatura em fahrenheit é: ${F}`)
