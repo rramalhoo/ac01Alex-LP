9c81361d2f4eaa63634ff73c6bf98da7be3442c0
Ramalho <159273388+rramalhoo@users.noreply.github.com>
2025-02-24 08:44:09 -0300

    ultimo commit

--- ex02/js/script.js
+++ ex02/js/script.js
@@ -2,4 +2,4 @@ celsius = parseFloat(prompt('Digite a temperatura em C:'));
 
 F = (celsius * 9 / 5) + 32;
 
-alert = (`A temperatura em fahrenheit é: ${F}`)
+alert(`A temperatura em fahrenheit é: ${F} °F`)
