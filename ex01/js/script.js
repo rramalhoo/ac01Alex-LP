3424e4f3483158a99301f3736937881222ba2805
rramalhoo <raphael.athenesi@gmail.com>
2025-02-24 07:44:34 -0300

    first commit

---
+++ ex01/js/script.js
@@ -0,0 +1,6 @@
+base = parseFloat(prompt('Digite a base:'))
+altura = parseFloat(prompt('Digite a altura:'))
+
+area = base * altura
+
+alert(`A area do retangulo é: ${area}`)
---
+++ ex02/js/script.js
@@ -0,0 +1,5 @@
+celsius = parseFloat(prompt('Digite a temperatura em C:'))
+
+convercao = (celsius * 9 / 5) = 32
+
+alert = (`A temperatura em fahrenheit é: ${convercao}`)
